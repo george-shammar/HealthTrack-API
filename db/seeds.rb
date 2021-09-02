7715c563a746dee270af33137f4740eeb253e7c2
george-shammar <george.gbenle@outlook.com>
2021-09-02 21:31:09 +0000

    Create faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Author.delete_all
+Article.delete_all
+
+
+10.times {
+    Author.create( name: Faker::Book.unique.author)
+}
+
+50.times {
+    Article.create({
+        title: Faker::Book.title,
+        body: Faker::Lorem.paragraphs(number: rand(5..7)),
+        author: Author.limit(1).order("RANDOM()").first # sql random
+    })
+}
