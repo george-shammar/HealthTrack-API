0b86b8d0b13f402fc112133e348d8e5b04d22fb3
george-shammar <george.gbenle@outlook.com>
2021-09-02 21:52:11 +0000

    Generate migration

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -1,3 +1,3 @@
 class Tracker < ApplicationRecord
-  belongs_to :User
+  belongs_to :user
 end
--- db/migrate/20210902212444_create_trackers.rb
+++ db/migrate/20210902212444_create_trackers.rb
@@ -3,7 +3,7 @@ class CreateTrackers < ActiveRecord::Migration[6.1]
     create_table :trackers do |t|
       t.integer :blood_pressure
       t.integer :blood_glucose
-      t.references :User, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
