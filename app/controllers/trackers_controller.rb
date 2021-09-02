9a5e124c2378bf1b2a9f14401859069cc1cf06f8
george-shammar <george.gbenle@outlook.com>
2021-09-02 22:28:39 +0000

    Update Trackers controller

--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -1,5 +1,5 @@
 class TrackersController < ApplicationController
-    before_action :find_tracker, only: :show
+    before_action :find_tracker, only: [:show, :update, :destroy]
     def index
         @trackers = Tracker.all
         render json: @trackers
@@ -9,7 +9,25 @@ class TrackersController < ApplicationController
         render json: @tracker
     end
 
+    def create
+        @tracker = current_user.trackers.create!(tracker_params)
+        json_response(@tracker, :created)
+    end
+    
+    def update
+        @tracker.update(tracker_params)
+        head :no_content
+    end
+    
+    def destroy
+        @tracker.destroy
+         head :no_content
+    end
+
     private
+      def tracker_params
+        params.permit(:blood_pressure, :blood_glucose)
+      end
         def find_tracker
             @tracker = Tracker.find(params[:id])
         end
