d39e320b7f85ca39d973ca85e13a6e3ae57e3a54
george-shammar <george.gbenle@outlook.com>
2021-09-02 22:01:38 +0000

    Implement tracker controller

--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -1,2 +1,16 @@
 class TrackersController < ApplicationController
+    before_action :find_tracker, only: :show
+    def index
+        @trackers = Tracker.all
+        render json: @trackers
+    end
+
+    def show
+        render json: @tracker
+    end
+
+    private
+        def find_tracker
+            @tracker = Tracker.find(params[:id])
+        end
 end
