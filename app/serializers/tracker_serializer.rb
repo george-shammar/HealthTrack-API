26b25ce76e91c634c4f5e325472b7dac14504a41
george-shammar <george.gbenle@outlook.com>
2021-09-05 19:46:11 +0000

    Implement tracker serializer

---
+++ app/serializers/tracker_serializer.rb
@@ -0,0 +1,10 @@
+class TrackerSerializer < ActiveModel::Serializer
+    attributes :id, :blood_pressure, :blood_glucose, :user
+
+    def user
+      {
+        id: object.user.id,
+        username: object.user.username
+      }
+    end
+end
