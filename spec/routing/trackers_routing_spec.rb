81e20b69d610962796e42fa57a89cdb3ba672d36
george-shammar <george.gbenle@outlook.com>
2021-09-17 21:57:20 +0000

    Create trackers routing specs

--- spec/routing/trackers_routing_spec.rb
+++ spec/routing/trackers_routing_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe TrackersController, type: :routing do
         it 'routes to #index' do
             expect(get: 'api/trackers').to route_to('trackers#index')
         end
+
+        it 'routes to #show' do
+            expect(get: 'api/trackers/1').to route_to('trackers#show', id: '1')
+        end
     end
 end
