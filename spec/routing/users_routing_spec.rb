40f7b5f0ac1c6f895470dc9dba7d293d38c95218
george-shammar <george.gbenle@outlook.com>
2021-09-17 21:40:38 +0000

    Create users routing spec

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :routing do
     describe 'routing' do
         it 'routes to #index' do
-        expect(get: 'api/users').to route_to('users#index')
-      end
+            expect(get: 'api/users').to route_to('users#index')
+        end
+
+        it 'routes to #show' do
+            expect(get: 'api/users/1').to route_to('users#show', id: '1')
+        end
     end
 end
