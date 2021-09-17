53bdcfc987f5b279f6ccea03b13138e91a5de7a3
george-shammar <george.gbenle@outlook.com>
2021-09-17 22:11:49 +0000

    Create trackers routing specs

--- spec/routing/trackers_routing_spec.rb
+++ spec/routing/trackers_routing_spec.rb
@@ -14,16 +14,32 @@ RSpec.describe TrackersController, type: :routing do
             expect(get: 'api/trackers/1').to route_to('trackers#show', id: '1')
         end
 
+        it 'does not routes to #index' do
+            expect(get: 'api/trackers/1').not_to route_to('trackers#index', id: '1')
+        end
+
         it 'routes to #create' do
             expect(post: 'api/trackers').to route_to('trackers#create')
+        end
+
+        it 'doex not routes to #show' do
+            expect(post: 'api/trackers').not_to route_to('trackers#show')
         end
 
         it 'routes to #update' do
             expect(put: 'api/trackers/1').to route_to('trackers#update', id: '1')
         end
 
+        it 'does not routes to #delete' do
+            expect(put: 'api/trackers/1').not_to route_to('trackers#delete', id: '1')
+        end
+
         it 'routes to #destroy' do
             expect(delete: 'api/trackers/1').to route_to('trackers#destroy', id: '1')
+        end
+
+        it 'does not routes to #update' do
+            expect(delete: 'api/trackers/1').not_to route_to('trackers#update', id: '1')
         end
     end
 end
--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -6,20 +6,40 @@ RSpec.describe UsersController, type: :routing do
             expect(get: 'api/users').to route_to('users#index')
         end
 
+        it 'does not routes to #show' do
+            expect(get: 'api/users').not_to route_to('users#show')
+        end
+
         it 'routes to #show' do
             expect(get: 'api/users/1').to route_to('users#show', id: '1')
+        end
+
+        it 'does not routes to #create' do
+            expect(get: 'api/users/1').not_to route_to('users#create', id: '1')
         end
 
         it 'routes to #create' do
             expect(post: 'api/users').to route_to('users#create')
         end
 
+        it 'does not routes to #index' do
+            expect(post: 'api/users').not_to route_to('users#index')
+        end
+
         it 'routes to #update' do
             expect(put: 'api/users/1').to route_to('users#update', id: '1')
         end
 
+        it 'does not routes to #create' do
+            expect(put: 'api/users/1').not_to route_to('users#create', id: '1')
+        end
+
         it 'routes to #destroy' do
             expect(delete: 'api/users/1').to route_to('users#destroy', id: '1')
+        end
+
+        it 'does not routes to #index' do
+            expect(delete: 'api/users/1').not_to route_to('users#index', id: '1')
         end
 
     end
